678e460231be4f2c981e99e9eefd2eae200c5648
enricojohn <hdpjenrico@gmail.com>
2020-07-17 01:01:25 -0300

    Adicionei a Comida

--- script.js
+++ script.js
@@ -4,15 +4,24 @@ let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
 snake[0] ={
-    x: 8 * box,
-    y: 8 * box
+    x: 7 * box,
+    y: 7 * box
 }
 
 let direction = "right"
+let food = {
+     x: Math.floor(Math.random() * 15 + 1) * box,  
+     y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
     context.fillStyle = 'lightgreen';
     context.fillRect(0,0,16 * box, 16 * box);
+}
+
+function drawFood() {
+    context.fillStyle = 'red';
+    context.fillRect(food.x, food.y, box, box)
 }
 
 function criarCobrinha() {
@@ -43,6 +52,7 @@ function iniciarJogo() {
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x
     let snakeY = snake[0].y
@@ -62,4 +72,4 @@ function iniciarJogo() {
     snake.unshift(newHead);
 }
 
-let jogo = setInterval(iniciarJogo, 100);
+let jogo = setInterval(iniciarJogo, 150);
